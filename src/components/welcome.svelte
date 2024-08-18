6a5c1676990bbd07ff8a222fc4aa24ad993841f9
Kenny Sheldon <kenny.sheldon99@gmail.com>
2024-08-18 16:11:35 -0500

    chore: reformat css for welcome animation

--- src/components/welcome.svelte
+++ src/components/welcome.svelte
@@ -46,7 +46,7 @@
 	});
 </script>
 
-<div class="flex flex-col pt-56">
+<div class="flex flex-col mt-36 h-auto">
 	<h1 class="font-mono text-6xl flex justify-center mb-8">{typedChars}</h1>
 	<h1 class="font-mono text-2xl flex justify-center">{subTypedChars}</h1>
 </div>
