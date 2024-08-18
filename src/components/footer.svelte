75c8cd06c69cf540c3fa73ce91548aaed7b97f20
Kenny Sheldon <kenny.sheldon99@gmail.com>
2024-08-18 14:05:06 -0500

    feat: initial commit

---
+++ src/components/footer.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { Terminal } from 'lucide-svelte';
+	import { Linkedin } from 'lucide-svelte';
+</script>
+
+<footer class="footer text-neutral-content items-center p-4 mt-auto">
+	<aside class="grid-flow-col items-center">
+		<Terminal />
+	</aside>
+	<nav class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+		<a href="https://www.x.com/DevOpsKenny" target="_blank">
+			<svg
+				xmlns="https://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				class="icon icon-tabler icons-tabler-outline icon-tabler-brand-x"
+			>
+				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+				<path d="M4 4l11.733 16h4.267l-11.733 -16z" />
+				<path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772" />
+			</svg>
+		</a>
+		<a href="https://www.linkedin.com/in/kbs56" target="_blank">
+			<Linkedin />
+		</a>
+	</nav>
+</footer>
---
+++ src/components/header.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { Github } from 'lucide-svelte';
+	import { SearchSlash } from 'lucide-svelte';
+</script>
+
+<nav>
+	<div class="navbar bg-base-100">
+		<div class="flex-1">
+			<a class="btn btn-ghost text-xl" href="/">ksheldon.dev</a>
+		</div>
+		<div class="flex-none">
+			<ul class="menu menu-horizontal px-1">
+				<li><a href="/about">About<SearchSlash /></a></li>
+				<li><a href="https://github.com/Kbs56" target="_blank">Github<Github /></a></li>
+				<!-- <li> -->
+				<!-- 	<details> -->
+				<!-- 		<summary>Parent</summary> -->
+				<!-- 		<ul class="bg-base-100 rounded-t-none p-2"> -->
+				<!-- 			<li><a href="https://github.com/Kbs56">Github</a></li> -->
+				<!-- 			<li><a href="/linkedin">Linkedin</a></li> -->
+				<!-- 		</ul> -->
+				<!-- 	</details> -->
+				<!-- </li> -->
+			</ul>
+		</div>
+	</div>
+</nav>
